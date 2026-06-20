0fe7db937a23c30e88c4e9681bca63915927ae96
Argolein <52103738+Argolein@users.noreply.github.com>
2026-06-20 13:26:12 +0200

    Add UI toggle to optionally disable preheating before unloading filament
    This commit introduces a new toggle in the Advanced Settings menu, "Preheat before unloading", allowing users to disable the forced preheat sequence that occurs during filament unloading.
    
    Changes:
    - Added `preheat_for_unloading` flag to the configuration store (enabled by default).
    - Implemented `MI_ADV_PREHEAT_FOR_UNLOADING` toggle switch in `ScreenMenuAdvancedSettings`.
    - Modified `M702_unload` to bypass the `preheat_to` block if the setting is disabled.
    - Updated cold extrusion prevention logic in `load_unload` to allow cold unloads without throwing errors if the user has explicitly disabled preheating.

--- src/gui/MItem_advanced_settings.cpp
+++ src/gui/MItem_advanced_settings.cpp
@@ -76,3 +76,11 @@ MI_ADV_STEPS_PER_UNIT_E::MI_ADV_STEPS_PER_UNIT_E()
 void MI_ADV_STEPS_PER_UNIT_E::OnClick() {
     store_and_apply_steps_per_unit(E_AXIS, GetVal());
 }
+
+MI_ADV_PREHEAT_FOR_UNLOADING::MI_ADV_PREHEAT_FOR_UNLOADING()
+    : WI_ICON_SWITCH_OFF_ON_t(config_store().preheat_for_unloading.get() ? 1 : 0, _(label), nullptr, is_enabled_t::yes, is_hidden_t::no) {}
+
+void MI_ADV_PREHEAT_FOR_UNLOADING::OnChange(size_t old_index) {
+    WI_ICON_SWITCH_OFF_ON_t::OnChange(old_index);
+    config_store().preheat_for_unloading.set(value());
+}
